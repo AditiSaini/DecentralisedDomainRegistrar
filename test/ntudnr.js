daaf6204858c86c70be641a4e0136fab05074599
Aditi <aditisaini99@gmail.com>
2020-11-02 18:22:20 +0800

    Updated UTs

--- test/ntudnr.js
+++ test/ntudnr.js
@@ -9,24 +9,14 @@ contract("NtuDNR Tests", async (accounts) => {
         assert.equal(owner, accounts[0]);
     });
 
-    it ("Search for an unregistered domain", async () =>{
+    it ("Search for an unregistered domain and should return nothing", async () =>{
         let ntudnr = await ntuDNR.deployed();
         let test_domain = "dnwang.ntu";
         let result = await ntudnr.searchDomain.call(test_domain);
         assert.equal(result,true)
 
     });
-/*
-    it("Can create an unregistered domain for an address", async () => {
-        let ntudnr = await ntuDNR.deployed();
-        let receiver_1 = accounts[1];
-        let test_domain = "dnwang.ntu";
-        await ntudnr.createDomain(receiver_1,test_domain, {from:accounts[0]});
-        let target_domain = await ntudnr.queryDomain.call(receiver_1);
-        console.log(target_domain);
-        assert.equal(target_domain, test_domain);
-    });
-*/
+
     it("Query an empty domain and should return empty string", async () =>{
         let ntudnr = await ntuDNR.deployed();
         let test_addr = accounts[0];
@@ -37,36 +27,31 @@ contract("NtuDNR Tests", async (accounts) => {
     it("First user can make a bid", async() =>{
         let ntudnr = await ntuDNR.deployed();
         let domain = "dnwang.ntu";
+        let bidValue = await ntudnr.valueInWei.call(5);
         await ntudnr.makeBid(domain, "0xd7146442058b5aadbcc239a52d5989a8369cdfdd83bb31bf88e29893823a7a52",
-            {from:accounts[0], value: 5});  // Hash value of 1 ether
+            {from:accounts[0], value: bidValue});  // Hash value of 1 ether
         let result = await ntudnr.viewBidDeposit.call(domain, accounts[0]);
-        assert.equal(result, 5);
+        assert.equal(result, 5*(10**18));
     });
 
     it("Second user can make a higher bid for the same domain", async() =>{
         let ntudnr = await ntuDNR.deployed();
         let domain = "dnwang.ntu";
+        let bidValue = await ntudnr.valueInWei.call(5);
         await ntudnr.makeBid(domain, "0xbe5cf62c4764fd2cfb9867d4cf56f14b6e14a9c0cf6cfb102ea36f9ba47725c9",
-            {from:accounts[1],value:5}); // Hash value of 3 ethers
+            {from:accounts[1],value:bidValue}); // Hash value of 3 ethers
         let result = await ntudnr.viewBidDeposit.call(domain,accounts[1]);
-        assert.equal(result, 5);
+        assert.equal(result, 5*(10**18));
     });
 
-    it("First user pass in an invalid value and should not get refund", async() =>{
-        let ntudnr = await ntuDNR.deployed();
-        let domain = "dnwang.ntu";
-        let result = await ntudnr.reveal.call(domain,0,false,"0xc89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6",
-            {from: accounts[0]});
-        assert.equal(result, 0);
-    });
-
-    it ("First user pass in a valid value and check the correct hash value", async() => {
+    it ("First user pass in a valid value and he should be authenticated", async() => {
         let ntudnr = await ntuDNR.deployed();
         let domain = "dnwang.ntu";
         let result = await ntudnr.checkHashValue.call(1, false, "0xc89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6",
             {from: accounts[0]});
-        assert.equal(result,"0xd7146442058b5aadbcc239a52d5989a8369cdfdd83bb31bf88e29893823a7a52");
-    })
+        let originalHash = await ntudnr.checkOrigHashValue.call(accounts[0],domain);
+        assert.equal(result,originalHash);
+    });
 
     it ("Can return correct blindBid made by user 1", async() =>{
         let ntudnr = await ntuDNR.deployed();
@@ -78,12 +63,12 @@ contract("NtuDNR Tests", async (accounts) => {
     it ("Both users pass in correct values and should return the correct top bid", async() =>{
         let ntudnr = await ntuDNR.deployed();
         let domain = "dnwang.ntu";
-        let result_0 = await ntudnr.reveal(domain,1,false,"0xc89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6",
+        await ntudnr.reveal(domain,1,false,"0xc89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6",
                 {from:accounts[0]});
-        let result_1 = await ntudnr.reveal(domain,3,false,"0x2a80e1ef1d7842f27f2e6be0972bb708b9a135c38860dbe73c27c3486c34f4de",
+        await ntudnr.reveal(domain,3,false,"0x2a80e1ef1d7842f27f2e6be0972bb708b9a135c38860dbe73c27c3486c34f4de",
             {from:accounts[1]});
         let result = await ntudnr.viewTopBidValue.call();
-        assert.equal(result, 3);
+        assert.equal(result, 3*(10**18));
 
     });
 
@@ -92,27 +77,13 @@ contract("NtuDNR Tests", async (accounts) => {
         let domain = "dnwang.ntu";
         let result = await ntudnr.queryAddress.call(domain);
         assert.equal(result,accounts[1]);
-    })
-
-    /*
-    it("First user pass in correct values and should return correct refund" , async() =>{
-        let ntudnr = await ntuDNR.deployed();
-        let result= await ntudnr.reveal(1,false,"0xc89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6",
-            {from:accounts[0]});
-        assert.equal(result.valueOf(), "0xc89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6");
-
     });
 
-        it("can withdraw less than despoited", async () => {
-            let bank = await Bank.deployed();
-            await bank.deposit({
-                from: accounts[0],
-                value: web3.utils.toWei("3"),
-            });
-            await bank.withdraw(web3.utils.toWei("2.9"), { from: accounts[0] });
+    it ("Domain should belong to user 2", async() =>{
+        let ntudnr = await ntuDNR.deployed();
+        let domain = "dnwang.ntu";
+        let result = await ntudnr.viewDomainOfAddress.call(accounts[1]);
+        assert.equal(result,domain);
+    });
 
-            let deposited = await bank.balance({ from: accounts[0] });
-            assert.equal(deposited.toString(), web3.utils.toWei("0.1"));
-        });
-    */
 });
